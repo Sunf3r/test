b3db054c1f11f59a7e2618b36eb6af268faefcd7
Sunf3r <sunf3r@proton.me>
2022-11-12 03:05:07 +0000

    undefined

--- Main.ts
+++ Main.ts
@@ -1,19 +1,13 @@
-function translate(data: string | BufferSource): string | Uint8Array {
-    if (typeof data === "string")
-        return new TextEncoder().encode(data);
-
-    return new TextDecoder().decode(data);
-}
-
 // setInterval(async () => {
-const files = ['test', 'bah', 'main', 'index', 'data', 'log', 'output', 'source', 'api', 'server'];
+const files = ['test', 'bah', 'main', 'index', 'data', 'log', 'output', 'source', 'api', 'server', 'prototypes'];
 for (const file of files) {
     const random = Math.random().toPrecision(50);
-    const data = translate(random + '\n') as Uint8Array;
+    const data = convert(String(convert(random + '\n'))) as Uint8Array;
     await Deno.writeFile(`src/${file}.ts`, data, { create: true });
 }
 
-const commands = ['git add .', `git commit -m ${Math.random().toFixed(5)}`, 'git push origin master'];
+const commits = ['small bug fixes', 'fixing bugs', 'small improvements', 'updating old code', 'cleaning up this trash']
+const commands = ['git add .', `git commit -m ${random(commits)}`, 'git push origin master'];
 
 for (const cmd of commands) {
     const process = Deno.run({
@@ -23,8 +17,8 @@ for (const cmd of commands) {
     });
     const status = await process.status();
 
-    const output = translate(await process.output());
-    const error = translate(await process.stderrOutput());
+    const output = convert(await process.output());
+    const error = convert(await process.stderrOutput());
 
     if (status.code === 0) console.log(output);
     else console.log(error);
@@ -32,3 +26,14 @@ for (const cmd of commands) {
     process.close();
 }
 // }, 1_000 * 60 * 0);
+
+function convert(data: string | BufferSource): string | Uint8Array {
+    if (typeof data === "string")
+        return new TextEncoder().encode(data);
+
+    return new TextDecoder().decode(data);
+}
+
+function random(array: string[]) {
+    return array[Math.random() * array.length];
+}
