68030c992c3d2215e14d69eb013672eb7aa864eb
Sunf3r <sunf3r@proton.me>
2022-11-12 03:44:11 +0000

    "commit1"

--- Main.ts
+++ Main.ts
@@ -1,37 +1,31 @@
 let count = 0;
 
 // setInterval(async () => {
-    const files = ['test', 'bah', 'main', 'index', 'data', 'log', 'output', 'source', 'api', 'server', 'prototypes'];
-    for (const file of files) {
-        const random = Math.random().toPrecision(50);
-        const data = convert(String(convert(random + '\n'))) as Uint8Array;
-        await Deno.writeFile(`src/${file}.ts`, data, { create: true });
-    }
+const files = ['test', 'bah', 'main', 'index', 'data', 'log', 'output', 'source', 'api', 'server', 'prototypes'];
+for (const file of files) {
+    const random = Math.random().toPrecision(50);
+    const data = convert(String(convert(random + '\n'))) as Uint8Array;
+    await Deno.writeFile(`src/${file}.ts`, data, { create: true });
+}
 
-    // const commits = ['small bug fixes', 'fixing bugs', 'small improvements', 'updating old code', 'cleaning up this trash']
-    const commands = ['deno fmt src/', 'git add .', `git commit -m "commit${++count}"`, 'git push origin master'];
+// const commits = ['small bug fixes', 'fixing bugs', 'small improvements', 'updating old code', 'cleaning up this trash']
+const commands = ['deno fmt src/', 'git add .', `git commit -m "commit${++count}"`, 'git push origin master'];
 
-    for (const cmd of commands) {
-        const process = Deno.run({
-            cmd: cmd.split(' '),
-            stderr: 'piped',
-            stdout: 'piped'
-        });
-        const status = await process.status();
+for (const cmd of commands) {
+    const process = Deno.run({
+        cmd: cmd.split(' '),
+        stderr: 'piped',
+        stdout: 'piped'
+    });
 
-        // const output = convert(await process.output());
-        const error = convert(await process.stderrOutput());
+    // const output = convert(await process.output());
+    const error = convert(await process.stderrOutput());
 
-        if (status.code === 1) {
-            const log = convert(`commit ${count} at ${getDate()}`) as Uint8Array;
-            await Deno.writeFile('success.log', log)
-        } else {
-            const log = convert(`commit ${count} at ${getDate()}\n - ${error}`) as Uint8Array;
-            await Deno.writeFile('error.log', log)
-        }
+    const log = convert(`commit ${count} at ${getDate()}\n - ${error}`) as Uint8Array;
+    await Deno.writeFile('log.txt', log)
 
-        process.close();
-    }
+    process.close();
+}
 // }, 1_000 * 60 * 60 * 4);
 //  1s    1m   1h   4h
 
@@ -45,7 +39,7 @@ function convert(data: string | BufferSource): string | Uint8Array {
 function getDate() {
     const now = new Date();
 
-    return `${now.getHours()}:${now.getMinutes()} - ${now.getDate()}/${now.getMonth()}`;
+    return `${now.getHours()}:${now.getMinutes()} - ${now.getDate()}/${now.getMonth() + 1}`;
 }
 
 // function random(array: string[]) {
